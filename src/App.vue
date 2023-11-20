f78736b396e9e18ff7a78f57655b1ef55b472c66
jeunh <gkwldms1218@gmail.com>
2023-11-20 22:52:33 +0900

    기본 코드 작성

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">change</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    setup() {
+      // data
+      const message = ref('hello');
+      
+      // methods
+      function changeMessage() {
+        message.value = 'hi';
+      }
+
+      return { message, changeMessage }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
